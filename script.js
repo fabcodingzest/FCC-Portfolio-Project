e1a103bfd864185331715c93a19ad88a31752e09
fabcodingzest <fabrizvi786@gmail.com>
2020-05-25 00:54:32 +0530

    About Section Completed

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 const menuBox = document.querySelector('.hamburger--menu');
 const navList = document.querySelector('.mobile__nav--list');
+const arrow = document.querySelector('.about__arrow');
 
 menuBox.addEventListener('click', e => {
   e.preventDefault();
   navList.classList.toggle('open');
   menuBox.classList.toggle('active');
 })
+
+arrow.addEventListener('click', e => {
+  e.preventDefault();
+  arrow.classList.toggle('up-arrow');
+  document.querySelector('.about__text').classList.toggle('full-about')
+} )
